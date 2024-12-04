7f92bafec054c012c8293bc95b593c9ec0108d09
douglastaylorb <douglastaylor20@gmail.com>
2024-12-04 17:51:16 -0300

    Improve logs

--- db/db.go
+++ db/db.go
@@ -1,9 +1,7 @@
 package db
 
 import (
-	"fmt"
-	"log"
-
+	"github.com/rs/zerolog/log"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -25,7 +23,7 @@ func Init() *gorm.DB {
 	db, err := gorm.Open(sqlite.Open("student.db"), &gorm.Config{})
 
 	if err != nil {
-		log.Fatalln(err)
+		log.Fatal().Err(err).Msgf("Failed to initialize SQLite: %s" + err.Error())
 	}
 
 	db.AutoMigrate(&Student{})
@@ -42,10 +40,11 @@ func NewStudentHandler(db *gorm.DB) *StudentHandler {
 func (s *StudentHandler) AddStudent(student Student) error {
 
 	if result := s.DB.Create(&student); result.Error != nil {
+		log.Error().Msg("Error adding student")
 		return result.Error
 	}
 
-	fmt.Println("Student Created")
+	log.Info().Msg("Student added successfully")
 	return nil
 }
 
